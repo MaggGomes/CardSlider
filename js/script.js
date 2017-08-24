a7c789f4f43d295826cfe9b680ba23e3f33f8554
MaggGomes <magg.slb@gmail.com>
2017-08-25 00:46:17 +0100

    card now is a reusable js component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
-/**
- * Created by Manuel Gomes on 24/08/2017.
- */
+$(document).ready(function (){
+
+
+    $.getJSON("http://localhost:3000/cards/", function( data ) {
+        $('.cardSlider').append('<div class="cards-container"></div>');
+
+
+        $.each(data, function(key, value) {
+            /* Reusable Card component*/
+            var $card = $(
+                '<div class="card">' +
+                    '<div class="card-image"></div>' +
+                    '<div class="card-content">' +
+                        '<div class="card-header">' +
+                            '<div class="card-logo"><img src="res/images/logo.png" alt="Logo"></div>' +
+                            '<div class="card-presentation">' +
+                                '<div class="title"></div>' +
+                                '<div class="description">what will you find here</div>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="card-body"></div>' +
+                        '<div class="card-footer">Learn More</div>' +
+                    '</div>' +
+                '</div>');
+
+
+            $card.find('.card-image').append('<img src=' + value.image_url + ' alt="Description">');
+            $card.find('.title').append(value.title);
+            $card.find('card-body').append(value.text);
+
+
+
+            $('.cardSlider .cards-container').append($card);
+        });
+
+
+
+
+
+
+        $('.cardSlider').append('<div class="cardSlider-footer">&lsaquo; &rsaquo;</div>');
+
+    });
+
+
+    $('.cardSlider-footer').click(function(){
+        $(this).css('background-color', 'red');
+    });
+
+});
