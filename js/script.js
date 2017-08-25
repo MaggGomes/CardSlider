915a05bad0a122b18088ebd6da70792ef5f2471a
MaggGomes <magg.slb@gmail.com>
2017-08-25 23:20:03 +0100

    carroussel now working

--- js/script.js
+++ js/script.js
@@ -4,95 +4,106 @@ $(document).ready(function (){
         url: "http://localhost:3000/cards/",
         numberSlides: 1
     });
+
+    $('#sliderExample2').cardSlider({
+        url: "http://localhost:3000/cards/",
+        numberSlides: 2
+    });
 });
 
 $.fn.cardSlider = function(options){
     var settings = $.extend({
-        // Default options.
+        /* Default settings */
         url: "http://localhost:3000/cards/",
         numberSlides: 1
     }, options);
 
-    console.log(settings.numberSlides);
     var slider = this;
-
-    $.getJSON(settings.url, function(data) {
-        slider.append('<div class="cards-container"></div>');
-
-        $.each(data, function(key, value) {
-            /* Reusable Card component*/
-            var $card = $(
-                '<div class="card">' +
-                '<div class="card-image"></div>' +
-                '<div class="card-content">' +
+    /* Reusable Card component*/
+    var $card = $(
+        '<div class="card">' +
+            '<div class="card-image"></div>' +
+            '<div class="card-content">' +
                 '<div class="card-header">' +
-                '<div class="card-logo"><img src="res/images/logo.png" alt="Logo"></div>' +
-                '<div class="card-presentation">' +
-                '<div class="title"></div>' +
-                '<div class="description">what will you find here</div>' +
-                '</div>' +
+                    '<div class="card-logo"><img src="res/images/logo.png" alt="Logo"></div>' +
+                    '<div class="card-presentation">' +
+                        '<div class="title"></div>' +
+                        '<div class="description">what will you find here</div>' +
+                    '</div>' +
                 '</div>' +
                 '<div class="card-body"></div>' +
                 '<div class="card-footer">Learn More</div>' +
-                '</div>' +
-                '</div>');
+            '</div>' +
+        '</div>');
 
-            $card.find('.card-image').append('<img src=' + value.image_url + ' alt="Description">');
-            $card.find('.title').append(value.title);
-            $card.find('.card-body').append(value.text);
+    /* Data request */
+    $.getJSON(settings.url, function(data){
+        slider.append('<div class="cards-container"></div>');
 
-            slider.find('.cards-container').append($card);
+        $.each(data, function(key, value) {
+            var $newCard = $card.clone();
+
+            $newCard.find('.card-image').append('<img src=' + value.image_url + ' alt="Description">');
+            $newCard.find('.title').append(value.title);
+            $newCard.find('.card-body').append(value.text);
+
+            slider.find('.cards-container').append($newCard);
         });
 
-        var slideIndex = 1;
-        showDivs(slideIndex);
+        slider.append('<div class="cardSlider-footer"><div class="cardSlider-arrows-wrapper"><button id="less" class="cardSlider-arrow">&#10094;</button> <button id="more" class="cardSlider-arrow">&#10095;</button></div></div>');
 
-        function plusDivs(n) {
-            showDivs(slideIndex += n);
+        var slides = slider.find('.card').length;
+
+        if (settings.numberSlides > 1){
+            var nextSlide = slider.find('.cards-container').children(':first-child');
+
+            nextSlide.clone().insertAfter(slider.find('.cards-container').children(':last-child'));
+
+            for (var x = 0; x < settings.numberSlides-2; x++){
+                nextSlide = nextSlide.next().clone();
+                nextSlide.insertAfter(slider.find('.cards-container').children(':last-child'));
+            }
         }
 
-        /*function showDivs(n) {
-            var i;
-            var x = slider.find('.card');
-            console.log(x.length);
-            if (n > x.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = x.length} ;
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-            }
+        var slideIndex = 1;
+        showDivs(slideIndex, slides);
 
-            x[slideIndex-1].style.display = "block";
-        }*/
+        function plusDivs(n, slides) {
+            showDivs(slideIndex += n, slides);
+        }
 
-        function showDivs(n){
+        function showDivs(n, slides){
             var i;
             var x = slider.find('.card');
 
-            var z = 2;
-
-            if (n > x.length){
+            if (n > slides){
                 slideIndex = 1
             }
 
             if (n < 1) {
-                slideIndex = x.length
+                slideIndex = x.length;
             }
 
             for (i = 0; i < x.length; i++){
                 x[i].style.display = "none";
             }
 
-            x[slideIndex-1].style.display = "block";
+            for (i = slideIndex; i < slideIndex+settings.numberSlides; i++){
+                x[i-1].style.display = "inline-block";
+            }
         }
 
-        slider.append('<div class="cardSlider-footer"><div class="cardSlider-arrows-wrapper"><button id="less" class="cardSlider-arrow">&#10094;</button> <button id="more" class="cardSlider-arrow">&#10095;</button></div></div>');
-
-        $('#less').click(function(){
-            plusDivs(-1);
+        slider.find('#less').click(function(){
+            plusDivs(-1, slides);
         });
-        $('#more').click(function(){
-            plusDivs(1);
+
+        slider.find('#more').click(function(){
+            plusDivs(1, slides);
         });
     });
 };
+
+
+
+
 
